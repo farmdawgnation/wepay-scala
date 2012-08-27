bd5510514e0321a648465cc243c411b23c32eec9
Matt Farmer <matt@frmr.me>
2012-08-26 22:47:23 -0400

    Add some basic API docs.

--- src/main/scala/me/frmr/wepay/WePay.scala
+++ src/main/scala/me/frmr/wepay/WePay.scala
@@ -9,17 +9,48 @@ package me.frmr.wepay {
     import liftjson.Js._
     import Http._
 
+  /**
+   * This class represents an error condition returned while
+   * attempting to run an operation against WePay's API.
+   *
+   * @param error The error type.
+   * @param error_description A friendlier, user readable error message.
+  **/
   case class WePayError(error:String, error_description:String) {
     override def toString = error + ": " + error_description
   }
 
+  /**
+   * An access token that authenticates your application to
+   * perform actions on behalf of a certain user on WePay's
+   * servers.
+   *
+   * @param user_id The ID of the uer this token relates to on WePay.
+   * @param access_token The actual token.
+   * @param token_type The type of token. Currently, always "BEARER"
+   * @param expires_in An expiration date associated with the token, if any.
+  **/
   case class WePayToken(user_id:Long, access_token:String, token_type:String, expires_in:Option[String]) {
+    /**
+     * The HTTP header to be sent with requests using this token.
+    **/
     val httpHeader = token_type.toLowerCase.capitalize + " " + access_token
   }
 
   case class WePayResponse(code:Int, json:JValue)
 
+  /**
+   * The Default WePay singleton with no additional functionality
+   * over WePayImpl.
+  **/
   object WePay extends WePayImpl
+
+  /**
+   * The base WePay implementation.
+   *
+   * This trait defines all the properties and functionality required
+   * to interact with the WePay API.
+  **/
   trait WePayImpl {
     implicit val formats = DefaultFormats
     // Authorization credentials for the application itself.
@@ -52,7 +83,10 @@ package me.frmr.wepay {
       }
     }
 
-    // Generate the OAuth2 Authorize URL
+    /**
+     * The URL that you should redirect your users to so that they may begin the
+     * OAuth workflow to authorize your application to act on their behalf.
+    **/
     val authorizeUrl = {
       for {
         clientId <- clientId
@@ -87,8 +121,12 @@ package me.frmr.wepay {
       }
     }
 
-    // Retrieve an OAuth token for a particular user based on the code
-    // provided from the OAuth success
+    /**
+     * Retrieve the WePayToken for a user based on a code passed into oauthRedirectUrl
+     * by WePay.
+     *
+     * @param oauthCode The code passed in by WePay in the code GET parameter.
+    **/
     def retrieveToken(oauthCode:String) : Box[WePayToken] = {
       def doRequest(clientId:String, redirectUrl:String, clientSecret:String, defaultHeaders:Map[String, String]) = {
         val requestBody : String = compact(render(
@@ -113,7 +151,17 @@ package me.frmr.wepay {
       }
     }
 
-    // Execute an action against the API.
+    /**
+     * Executes some action against the WePay API.
+     *
+     * This is a raw request that will take in a lift-json JValue as the request and
+     * produce a Box[JValue] as a response.
+     *
+     * @param accessToken The WePayToken associated with this request, if any.
+     * @param module The module associated with this request. So if the URL you wanted to hit was /checkout/create, this would be "checkout".
+     * @param action The action associated wiht this request. For "/checkout/create" it would be "create". Set to None for no action.
+     * @param requestJson The request JSON to be transmitted in the body of the post, if any.
+    **/
     def executeAction(accessToken:Option[WePayToken], module:String, action:Option[String], requestJson:JValue) : Box[JValue] = {
       def doRequest(defaultHeaders:Map[String, String]) = {
         val requestTarget = action.toList.foldLeft(apiEndpointBase / apiVersion / module)(_ / _).secure
