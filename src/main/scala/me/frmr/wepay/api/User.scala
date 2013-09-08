94468e3cb5e2a562be2cae6843c4ace8b20bfc2e
Matt Farmer <matt@frmr.me>
2013-09-07 22:05:55 -0400

    Get user resource working.

--- src/main/scala/me/frmr/wepay/api/User.scala
+++ src/main/scala/me/frmr/wepay/api/User.scala
@@ -8,17 +8,17 @@ package me.frmr.wepay.api {
    *
    * These are read only, and the use of their data is restricted by the WePay Developer TOS.
    *
-   * @param user_id The user's unique ID.
-   * @param first_name The user's first name.
-   * @param last_name The user's last name.
+   * @param userId The user's unique ID.
+   * @param firstName The user's first name.
+   * @param lastName The user's last name.
    * @param email The user's email.
    * @param state The state of the User's account.
   **/
-  case class User(user_id:Long, first_name:String, last_name:String, email:String, state:String)
+  case class User(userId:Long, firstName:String, lastName:String, email:String, state:String)
     extends WePayResource[User] {
 
     val meta = User
-    val _id = user_id
+    val _id = userId
   }
 
   /**
